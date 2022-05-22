5518216b485b45054ee2694c6ea07c714a296fb9
RaaM <luciferusss33@gmail.com>
2022-05-22 23:50:18 +0300

    .

--- controllers/auth.js
+++ controllers/auth.js
@@ -104,7 +104,7 @@ exports.register = (req, res) => {
  
 
     });
-    db.query('INSERT INTO stats SET id.stats = (SELECT id.users FROM users WHERE email = ?)',[email], (error, results)=>{
+    db.query('INSERT INTO stats SELECT id FROM users WHERE email = ?',[email], (error, results)=>{
         if(error){
             console.log(error);
         } else {
