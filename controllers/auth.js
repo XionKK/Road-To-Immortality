ff89c0947b3e1439884cb1fcb36ed4a87897bb9a
RaaM <luciferusss33@gmail.com>
2022-05-23 00:16:01 +0300

    .

--- controllers/auth.js
+++ controllers/auth.js
@@ -101,16 +101,16 @@ exports.register = (req, res) => {
                 console.log(results);
             }
         });*/
- 
-
-    });
-    db.query('INSERT INTO stats(id) SELECT id FROM users WHERE email = ?',[email], (error, results)=>{
+     db.query('INSERT INTO stats(id) SELECT id FROM users WHERE email = ?',[email], (error, results)=>{
         if(error){
             console.log(error);
         } else {
             console.log(results);
         }
     });
+
+    });
+
 
 }
 
