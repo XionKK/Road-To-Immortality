821f499e2781cbc65c83929dad4f1f8ad6c42884
RaaM <luciferusss33@gmail.com>
2022-05-23 22:29:12 +0300

    .

--- scripts/pagetabs.js
+++ scripts/pagetabs.js
@@ -1,25 +1,23 @@
-const express = require('express');
-
 function openPage(pageName, elmnt, color) {
     // Hide all elements with class="tabcontent" by default */
     var i, tabcontent, tablinks;
-    tabcontent = Document.getElementsByClassName("tabcontent");
+    tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
       tabcontent[i].style.display = "none";
     }
   
     // Remove the background color of all tablinks/buttons
-    tablinks = Document.getElementsByClassName("tablink");
+    tablinks = document.getElementsByClassName("tablink");
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].style.backgroundColor = "";
     }
   
     // Show the specific tab content
-    Document.getElementById(pageName).style.display = "block";
+    document.getElementById(pageName).style.display = "block";
   
     // Add the specific color to the button used to open the tab content
     elmnt.style.backgroundColor = color;
   }
   
   // Get the element with id="defaultOpen" and click on it
-  Document.getElementById("defaultOpen").click();
+  document.getElementById("defaultOpen").click();
